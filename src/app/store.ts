5cc14beb3029cfbd694776bd06215a55ef673f0b
Mark Xu <isafesoft@hotmail.com>
2018-10-16 21:24:49 -0400

    create store for app global use

---
+++ src/app/store.ts
@@ -0,0 +1,28 @@
+import { distinctUntilChanged } from 'rxjs/operators';
+import {BehaviorSubject, Observable} from 'rxjs';
+import { pluck } from 'rxjs/operators';
+
+export interface State {
+  [key: string]: any
+}
+
+const state: State = {};
+
+export class Store {
+
+  private subject = new BehaviorSubject<State>(state);
+  private store = this.subject.asObservable().pipe(distinctUntilChanged())
+
+  get value() {
+    return this.subject.value;
+  }
+
+  select<T>(name: string): Observable<T> {
+    return this.store.pipe(pluck(name))
+  }
+
+  set(name: string, state: any) {
+    this.subject.next({ ...this.value, [name]: state });
+  }
+
+}
