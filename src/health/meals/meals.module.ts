786e88bed8d6f256a4609caa8aec086893ae41f5
Mark Xu <isafesoft@hotmail.com>
2018-11-14 16:29:43 -0500

    9-Meal Component and async pipe template

--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -4,9 +4,11 @@ import { MealsComponent } from './containers/meals/meals.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {RouterModule, Routes} from '@angular/router';
 import {DataModule} from '../data/data.module';
+import { MealComponent } from './containers/meal/meal.component';
 
 export const ROUTES: Routes = [
-  {path: '', component: MealsComponent}
+  {path: '', component: MealsComponent},
+  {path: 'new', component: MealComponent},
 ]
 @NgModule({
   imports: [
@@ -15,6 +17,6 @@ export const ROUTES: Routes = [
     RouterModule.forChild(ROUTES),
     DataModule.forRoot()
   ],
-  declarations: [MealsComponent]
+  declarations: [MealsComponent, MealComponent]
 })
 export class MealsModule { }
